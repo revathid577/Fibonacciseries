1935c0703d7cb11c97a2da71ebd050dc74f744d6
revathi dasari <drrn531@gmail.com>
2024-03-08 16:20:37 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter length of Fibonacci series:");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out int length))
+        {
+            if (length > 0)
+            {
+                int a = 0, b = 1;
+                Console.Write("Fibonacci series: ");
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write($"{a} ");
+                    int temp = a + b;
+                    a = b;
+                    b = temp;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+}
